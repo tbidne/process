dacf74155b0406320903d2a794bc8808980b1266
Tommy Bidne <tbidne@protonmail.com>
2025-04-14 10:48:39 +1200

    Add System.Process.Environment(.OsString) modules
    Bumps lower bounds for base, filepath, unix, and Win32. Drops support
    for GHC < 8.6, as required by newer Win32.

---
+++ System/Process/Environment.hs
@@ -0,0 +1,10 @@
+-- | Miscellaneous information about the system environment.
+--
+-- @since X.Y.Z
+module System.Process.Environment (
+    Env.getArgs,
+    Env.getEnv,
+    Env.getEnvironment,
+    ) where
+
+import qualified System.Environment as Env
